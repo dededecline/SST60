c80222e3eb64ed430c080924e7a4897362e160ba
Olivia <oliviaj@Olivias-Mac-Pro.local>
2022-08-27 16:20:12 +0300

    changed firmware to utilize RGB Matrix for vial

--- Firmware/Source/sst60/rules.mk
+++ Firmware/Source/sst60/rules.mk
@@ -7,12 +7,19 @@ BOOTLOADER = rp2040
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes	    # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no	    # Mouse keys
+MOUSEKEY_ENABLE = yes	    # Mouse keys
 EXTRAKEY_ENABLE = yes	    # Audio control and System control
 CONSOLE_ENABLE = no	        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no		    # USB Nkey Rollover
+NKRO_ENABLE = yes		    # USB Nkey Rollover
 BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes         # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+
+#RP2040 Options
+ALLOW_WARNINGS = yes
+
+RGB_MATRIX_SUPPORTED = yes
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+
 WS2812_DRIVER = vendor
